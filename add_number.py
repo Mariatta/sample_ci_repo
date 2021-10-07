c8922b47b581fab12a1ad3bcfc5012060caba134
Mariatta Wijaya <mariatta.wijaya@uplight.com>
2021-10-07 14:37:42 -0700

    Add GitHub Actions to run tests

---
+++ add_number.py
@@ -0,0 +1,11 @@
+"""
+A utility for summing up two numbers.
+"""
+
+
+def add_two_numbers(first, second):
+    """Adds up both numbers and return the sum.
+    Input values must be numbers."""
+    if not isinstance(first, (int, float)) or not (isinstance(second, (int, float))):
+        raise ValueError("Inputs must be numbers.")
+    return first + second
---
+++ test_add_number.py
@@ -0,0 +1,24 @@
+import unittest
+from add_number import add_two_numbers
+
+
+class TestAddNumbers(unittest.TestCase):
+    def test_add_two_numbers(self):
+        result = add_two_numbers(1, 2)
+        self.assertEqual(result, 3)
+
+    def test_add_two_negative_numbers(self):
+        result = add_two_numbers(-1, -2)
+        self.assertEqual(result, -3)
+
+    def test_add_decimals_numbers(self):
+        result = add_two_numbers(10.5, 3.25)
+        self.assertEqual(result, 13.75)
+
+    def test_add_letters(self):
+        with self.assertRaises(ValueError):
+            add_two_numbers("A", "B")
+
+    def test_add_letter_and_number(self):
+        with self.assertRaises(ValueError):
+            add_two_numbers(1, "B")
